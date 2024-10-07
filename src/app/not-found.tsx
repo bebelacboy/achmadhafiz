39d2f3c5ae6c3709150d707567baea2d9752b53f
Achmad Hafiz <achmadhafiz99@gmail.com>
2024-10-07 23:59:13 +0700

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="text-4xl -translate-y-14"><b>404</b> Not Found</h1>
+    </div>
+  )
+}
